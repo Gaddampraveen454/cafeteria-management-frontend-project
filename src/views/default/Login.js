8ec07a81a3d735f0cbd4b3a513289c5a67d3dc1b
Saibharath_14 <saibharath.kasireddy@scienstechnologies.com>
2023-10-10 18:48:49 +0530

    bg image changed

--- src/views/default/Login.js
+++ src/views/default/Login.js
@@ -87,19 +87,20 @@ const Login = () => {
     <div className="min-h-100 d-flex align-items-center">
       <div className="w-100 w-lg-75 w-xxl-50">
         <div>
-          <div className="mb-5">
-            <h1 className="display-3 text-white">Multiple Niches</h1>
-            <h1 className="display-3 text-white">Ready for Your Project</h1>
-          </div>
-          <p className="h6 text-white lh-1-5 mb-5">
-            Dynamically target high-payoff intellectual capital for customized technologies. Objectively integrate emerging core competencies before
-            process-centric communities...
-          </p>
-          <div className="mb-5">
+          {/* <div className="mb-5"> */}
+            <h3 className="display-3 text-white" style={{fontSize: "1.90em"}}> Comprehensive solution to address all your cafeteria management requirements in one place</h3>
+            {/* <h1 className="display-3 text-white">Ready for Your Project</h1> */}
+          {/* </div> */}
+          {/* <p className="h6 text-white lh-1-5 mb-5"> */}
+            {/* Dynamically target high-payoff intellectual capital for customized technologies. Objectively integrate emerging core competencies before
+            process-centric communities... */}
+           
+          {/* </p> */}
+          {/* <div className="mb-5">
             <Button size="lg" variant="outline-white" href="/">
               Learn More
             </Button>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
