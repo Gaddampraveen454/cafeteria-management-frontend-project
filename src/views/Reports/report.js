5cd9cf5e3dba08d4180c2d8b4a3ebfb1a4f6de25
bharadwaj <bharadwaj@scienstechnologies.com>
2023-11-15 11:51:45 +0530

    changes

--- src/views/Reports/report.js
+++ src/views/Reports/report.js
@@ -28,7 +28,7 @@ const report = () => {
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
 
-  const [storeuuid,setstoreuuid] = useState()
+  const [storeuuid, setstoreuuid] = useState('')
 
   const allItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const [selectedItems, setSelectedItems] = useState([]);
@@ -76,7 +76,7 @@ const report = () => {
   const Handlereportstore = (event) => {
     console.log(event?.value, "eventeventevent")
     setstoreuuid(event?.value)
-    dispatch(AdminReportListURL(page, search, currentUser?.token, limit, currentUser?.data?.uuid, event?.value,startDate, endDate))
+    dispatch(AdminReportListURL(page, search, currentUser?.token, limit, currentUser?.data?.uuid, event?.value, startDate, endDate))
 
   }
 
@@ -87,7 +87,7 @@ const report = () => {
     dispatch(ReportstorelistApi(page, search, currentUser.token, limit, currentUser?.data?.uuid))
   }, [])
   useEffect(() => {
-    dispatch(AdminReportListURL(page, search, currentUser?.token, limit, currentUser?.data?.uuid, "","",""))
+    dispatch(AdminReportListURL(page, search, currentUser?.token, limit, currentUser?.data?.uuid, storeuuid, startDate, endDate))
     // dispatch(ExportAdminReportURL(selectValueState && selectValueState.value, startDate, endDate, currentUser.token))
   }, [])
 
@@ -97,17 +97,17 @@ const report = () => {
   const ChangeStartData = e => {
     console.log("ChangeStartData: ", e.target.value);
     setStartDate(e.target.value);
-    dispatch(AdminReportListURL(page, search, currentUser?.token, limit, currentUser?.data?.uuid, storeuuid ,e.target.value,""))
+    dispatch(AdminReportListURL(page, search, currentUser?.token, limit, currentUser?.data?.uuid, storeuuid, e.target.value, endDate))
   };
   const ChangeEndData = e => {
     console.log("ChangeStartData: ", e.target.value);
     setEndDate(e.target.value);
-    dispatch(AdminReportListURL(page, search, currentUser?.token, limit, currentUser?.data?.uuid, storeuuid,startDate,e.target.value))
+    dispatch(AdminReportListURL(page, search, currentUser?.token, limit, currentUser?.data?.uuid, storeuuid, startDate, e.target.value))
   };
- 
-  const exportfunction = async () => {
-    await ExportExcel(`/report/list/admin/export?pagenum=0&limit=10&search=&company_uuid=${selectValueState && selectValueState.value}&user_uuid=&strat_date=${startDate}&end_date=${endDate}`, "Report", currentUser.token)
-  }
+
+  // const exportfunction = async () => {
+  //   await ExportExcel(`/report/list/admin/export?pagenum=0&limit=10&search=&company_uuid=${selectValueState && selectValueState.value}&user_uuid=&strat_date=${startDate}&end_date=${endDate}`, "Report", currentUser.token)
+  // }
 
   const searchfunction = (type, pages) => {
     console.log(pages, "ghjsdfsdfkvbnm")
@@ -115,32 +115,32 @@ const report = () => {
       console.log(pages, "ghjkvbnm")
       setSearch(pages)
       setPage(0)
-      dispatch(AdminReportListURL(0, pages, currentUser.token, limit, currentUser?.data?.uuid, storeuuid,startDate,endDate))
+      dispatch(AdminReportListURL(0, pages, currentUser.token, limit, currentUser?.data?.uuid, storeuuid, startDate, endDate))
     }
     if (type === "prev") {
       setPage(page - 1)
-      dispatch(AdminReportListURL(page - 1, search, currentUser.token, limit, currentUser?.data?.uuid, storeuuid,startDate,endDate))
+      dispatch(AdminReportListURL(page - 1, search, currentUser.token, limit, currentUser?.data?.uuid, storeuuid, startDate, endDate))
     }
     else if (type === "next") {
       setPage(page + 1)
-      dispatch(AdminReportListURL(page + 1, search, currentUser.token, limit, currentUser?.data?.uuid, storeuuid,startDate,endDate))
+      dispatch(AdminReportListURL(page + 1, search, currentUser.token, limit, currentUser?.data?.uuid, storeuuid, startDate, endDate))
     }
     else if (type === "page") {
       setPage(page)
-      dispatch(AdminReportListURL(page, search, currentUser.token, limit, currentUser?.data?.uuid, storeuuid,startDate,endDate))
+      dispatch(AdminReportListURL(page, search, currentUser.token, limit, currentUser?.data?.uuid, storeuuid, startDate, endDate))
     }
     else if (type === "page+1") {
       setPage(page + 1)
-      dispatch(AdminReportListURL(page + 1, search, currentUser.token, limit, currentUser?.data?.uuid, storeuuid,startDate,endDate))
+      dispatch(AdminReportListURL(page + 1, search, currentUser.token, limit, currentUser?.data?.uuid, storeuuid, startDate, endDate))
     }
     else if (type === "page+2") {
       setPage(page + 2)
-      dispatch(AdminReportListURL(page + 2, search, currentUser.token, limit, currentUser?.data?.uuid, storeuuid,startDate,endDate))
+      dispatch(AdminReportListURL(page + 2, search, currentUser.token, limit, currentUser?.data?.uuid, storeuuid, startDate, endDate))
     }
     else if (type === "limit") {
       setLimit(pages)
       setPage(0)
-      dispatch(AdminReportListURL(0, search, currentUser.token, pages, currentUser?.data?.uuid, storeuuid,startDate,endDate))
+      dispatch(AdminReportListURL(0, search, currentUser.token, pages, currentUser?.data?.uuid, storeuuid, startDate, endDate))
     }
   }
   return (
@@ -217,14 +217,14 @@ const report = () => {
           // disabled={eventType}
           />
         </Col>
-        <Col md="2" lg="2" xxl="2" className="mb-1" style={{marginTop:"-2%"}}>
+        <Col md="2" lg="2" xxl="2" className="mb-1" style={{ marginTop: "-2%" }}>
           {/* <div className="mb-3"> */}
           <Form.Label>Start date</Form.Label>
-          <Form.Control type="date" value={startDate} onChange={ChangeStartData}  placeholder="Start date"/>
+          <Form.Control type="date" value={startDate} onChange={ChangeStartData} placeholder="Start date" />
         </Col>
-        <Col md="2" lg="2" xxl="2" className="mb-1" style={{marginTop:"-2%"}}>
+        <Col md="2" lg="2" xxl="2" className="mb-1" style={{ marginTop: "-2%" }}>
           <Form.Label>End date</Form.Label>
-          <Form.Control type="date" value={endDate} onChange={ChangeEndData}  placeholder="End date"/>
+          <Form.Control type="date" value={endDate} onChange={ChangeEndData} placeholder="End date" />
           {/* </div> */}
         </Col>
         {/* <Col md="3" lg="3" xxl="3" className="mb-1">
